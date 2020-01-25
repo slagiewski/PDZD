919094f8b6f565642b330e5b1420c5a891ebee63
221523 <221523@student.pwr.edu.pl>
2020-01-25 19:59:25 +0100

    fix missing directory

--- processing/mapreduce/compile.sh
+++ processing/mapreduce/compile.sh
@@ -3,6 +3,8 @@
 set -eu
 CLASS_PATH="./lib/hadoop-common-2.7.4.jar:./lib/hadoop-mapreduce-client-core-2.7.4.jar:."
 
+mkdir -p out
+
 javac -cp $CLASS_PATH src/PhotosAdvanced.java -d out 
 cd out
 jar cvf photosadvanced.jar *.class
